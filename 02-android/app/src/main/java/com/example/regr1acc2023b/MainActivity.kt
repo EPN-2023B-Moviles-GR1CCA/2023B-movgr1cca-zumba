aa0abfac63b4d9f7ab9d53fec8da9b707619cf1f
EstherZumba <raquel.zumba@epn.edu.ec>
2023-12-09 16:33:42 -0500

    Subiendo archivos strings en la carpata res

--- 02-android/app/src/main/java/com/example/regr1acc2023b/MainActivity.kt
+++ 02-android/app/src/main/java/com/example/regr1acc2023b/MainActivity.kt
@@ -12,32 +12,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.regr1acc2023b.ui.theme.REgr1acc2023BTheme
 
-class MainActivity : ComponentActivity() {
+import  androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            REgr1acc2023BTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
-        }
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    REgr1acc2023BTheme {
-        Greeting("Android")
-    }
-}
