1732506e829b2adefd132f0393e8df23c855f1e4
EstherZumba <raquel.zumba@epn.edu.ec>
2024-02-12 16:00:04 -0500

    Realizando cambios en las actividades

--- 06-Deber_03/app/src/main/java/com/example/onedrive_raquelzumba/ArchivoAct.kt
+++ 06-Deber_03/app/src/main/java/com/example/onedrive_raquelzumba/ArchivoAct.kt
@@ -18,6 +18,16 @@ class ArchivoAct : AppCompatActivity() {
         listaArchivos.add(Archivo(R.drawable.ic_baseline_feed_24,"Tarea 7","25 MB","Nov 17, 2022"))
         listaArchivos.add(Archivo(R.drawable.ic_baseline_feed_24,"Tarea 8","75 MB","Mar 27, 2022"))
         listaArchivos.add(Archivo(R.drawable.ic_baseline_width_wide_24,"Tarea 9","80 MB","Ene 11, 2024"))
+        listaArchivos.add(Archivo(R.drawable.ic_baseline_feed_24,"Tarea 10","25 MB","Feb 17, 2024"))
+        listaArchivos.add(Archivo(R.drawable.ic_baseline_feed_24,"Tarea 11","75 MB","Jun 17, 2024"))
+        listaArchivos.add(Archivo(R.drawable.ic_baseline_feed_24,"Tarea 12","80 MB","Mar 10, 2021"))
+        listaArchivos.add(Archivo(R.drawable.ic_baseline_dataset_24,"Tarea 13","25 MB","Mar 17, 2020"))
+        listaArchivos.add(Archivo(R.drawable.ic_baseline_description_24,"Tarea 14","75 MB","Jul 17, 2020"))
+        listaArchivos.add(Archivo(R.drawable.ic_baseline_image_24,"Tarea 15","80 MB","Oct 10, 2023"))
+        listaArchivos.add(Archivo(R.drawable.ic_baseline_feed_24,"Tarea 16","25 MB","Nov 17, 2022"))
+        listaArchivos.add(Archivo(R.drawable.ic_baseline_feed_24,"Tarea 17","75 MB","Mar 27, 2022"))
+        listaArchivos.add(Archivo(R.drawable.ic_baseline_width_wide_24,"Tarea 18","80 MB","Ene 11, 2024"))
+
 
         val recyclerView = findViewById<RecyclerView>(R.id.rvArchivo)
         initRecyclerView(listaArchivos, recyclerView)
